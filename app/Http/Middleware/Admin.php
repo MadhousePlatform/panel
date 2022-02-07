68f7d579bd833d9aa80bf6bb2a6271b4abd7d334
Denver F <denverfreeburn@outlook.com>
2022-02-07 08:42:25 +0000

    Added admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $user = (new User)->whereId(auth()->id())->first();
+        if ( $user->admin !== null) {
+            return $next($request);
+        }
+
+        return redirect()->route('index');
+    }
+}
